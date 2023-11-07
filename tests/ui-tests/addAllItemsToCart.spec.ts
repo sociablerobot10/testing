b857b4ac2054f5eccc4adee681da91ac6cb4bc63
Joe DeFilippo <joedefilippo@users.noreply.github.com>
2023-11-06 20:27:01 -0500

    initial commit

---
+++ tests/ui-tests/addAllItemsToCart.spec.ts
@@ -0,0 +1,19 @@
+import { LoginPage } from "../../page-objects/login.page";
+import { Product } from "../../page-objects/product.page";
+import { test, expect } from "@playwright/test";
+
+test("add all items to cart", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  const loginPage = new LoginPage(page);
+  await loginPage.signIn(page, "standard_user", "secret_sauce");
+
+  const productPage = new Product(page);
+  const products = await page.getByRole('button').filter({ hasText: 'Add to cart' }).all();
+
+  for (let i = 0; i < products.length; i++) {
+    await products[0].click();
+  }
+
+  await expect(productPage.shoppingCartButton).toHaveText((products.length).toString());
+
+});
